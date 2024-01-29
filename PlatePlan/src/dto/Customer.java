7145f9ab31418eeaab795fa8c18110c2d122a14f
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2024-01-29 16:58:25 -0500

    Switched Project to Maven

--- PlatePlan/src/dto/Customer.java
+++ PlatePlan/src/dto/Customer.java
@@ -1,6 +1,13 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+	
 	
 	private String firstName;
 	private String lastName;
@@ -9,6 +16,29 @@ public class Customer {
 	
 	
 	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
-
 }
