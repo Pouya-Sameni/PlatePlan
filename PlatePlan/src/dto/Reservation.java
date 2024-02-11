d229cfcaf9b3a076bc18cba67eff4a2e76a07da1
droxuz <156976891+droxuz@users.noreply.github.com>
2024-02-11 16:10:46 -0500

    Object, Constructors(Server, Table, Reservation)
    Created ArrayList to hold multiple objects of Reservations, created constructors for server and table, and created 1 object of each type.

--- PlatePlan/src/dto/Reservation.java
+++ PlatePlan/src/dto/Reservation.java
@@ -5,10 +5,11 @@ package dto;
 
 import java.time.LocalDateTime;
 
-
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
@@ -16,20 +17,24 @@ import lombok.ToString;
 @ToString
 public class Reservation {
 
-	@NonNull
+	@NonNull @Getter @Setter
 	private String customerId;
-	@NonNull
-	private LocalDateTime  reservationTime;
-	
+	@NonNull @Getter @Setter
+	private LocalDateTime reservationTime;
+	@NonNull @Getter @Setter
 	private String specialNotes;
-	
+	@NonNull @Getter @Setter
 	private Server server;
-	
+	@NonNull @Getter @Setter
 	private Table table;
 
-	
-	
-
-
+	public Reservation(String customerId, String specialNotes, Server server, Table table) {
+		this.customerId = customerId;
+		this.specialNotes = specialNotes;
+		this.server = server;
+		this.table = table;
+		this.reservationTime = LocalDateTime.now();
+				
+	}
 
 }
